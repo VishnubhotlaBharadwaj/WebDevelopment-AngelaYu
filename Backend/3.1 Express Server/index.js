d7424c31fbef42eab65436ec527bd09c1f6fe113
Bharadwaj Vishnubhotla <vbharadwaj3456@gmail.com>
2024-02-20 12:19:23 -0500

    Update index.js

--- Backend/3.1 Express Server/index.js
+++ Backend/3.1 Express Server/index.js
@@ -2,6 +2,18 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+    res.send("<h1>Hey there!</h1>");
+});
+
+app.get("/about", (req, res) => {
+    res.send("<h1>About me</h1><p>I am Bharadwaj</p>");
+});
+
+app.get("/contact", (req, res) => {
+    res.send("<h1>Contact!</h1>");
+});
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`);
 })
